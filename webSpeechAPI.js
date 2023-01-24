14739aa086b5e1e1c79726457712779d6a170bc2
ShuneLaeMay <shunelaemay001@gmail.com>
2023-01-24 06:38:39 -0800

    third commit

--- webSpeechAPI.js
+++ webSpeechAPI.js
@@ -1,16 +1,62 @@
 const textToSpeech = document.querySelector("#textToSpeech");
+const speechToText = document.querySelector("#speechToText");
 const text = document.querySelector("#text");
-const synth = window.speechSynthesis;
 
+//Speech To Text
+const listen = () =>{
+    //Set the language and start recognizing
+    var recognition = new webkitSpeechRecognition();
+    recognition.lang = "en-US";
+    recognition.start();
+
+    //loading
+    recognition.addEventListener("start",() => {
+        speechToText.classList.add("active");
+        speechToText.innerHTML = `
+        <div class="spinner-border text-white spinner-border-sm" role="status">
+  <span class="visually-hidden">Loading...</span>
+</div>`;
+    });
+
+    recognition.addEventListener("end" ,() => {
+        speechToText.classList.remove("active");
+        recognition.innerHTML = ` 
+        <i class="bi bi-mic"></i>
+        `
+    })
+
+    //When a speech result is returned 
+    recognition.onresult = function (event) {
+    //Get the transcript of the spoken words
+    console.log(event.results);
+    var transcript = event.results[0][0].transcript;
+    text.value += transcript; //for multiple speech
+    //console.log(transcript);
+    }
+};
+
+//Text To Speech
+const synth = window.speechSynthesis;   //web speak
 const speak = (text) => {
-    const utterThis = new SpeechSynthesisUtterance(text);
+   // const utterThis = new SpeechSynthesisUtterance(text); //config user text speak
+   const utterThis = new SpeechSynthesisUtterance();
+   utterThis.rate = 0.8;
+   utterThis.text = text;
+   utterThis.voice = synth.getVoices()[5];
+
+   utterThis.addEventListener("start" , () => {
+    textToSpeech.classList.add("active");
+   })
+   utterThis.addEventListener("end" , () => {
+    textToSpeech.classList.remove("active");
+   })
+
     synth.speak(utterThis);
 }
 
-speak("hello I am here");
-
-
+//speak("hello I am here");
 textToSpeech.addEventListener("click",() => {
     speak(text.value);
     console.log("i love you")
-} )
+} );
+speechToText.addEventListener("click",listen);
